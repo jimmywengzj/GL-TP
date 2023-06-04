a8d0275f7d924138a50e51c7618cc8d78aa7e798
arosard <alexandre@rosard.info>
2023-06-04 20:53:36 +0200

    Mise en forme de la classe Provider

--- src/Provider.cpp
+++ src/Provider.cpp
@@ -13,24 +13,21 @@
 //------------------------------------------------------------- System includes
 using namespace std;
 #include <iostream>
+#include <map>
+#include <string>
 
 //----------------------------------------------------------- Personal includes
 #include "Provider.h"
-#include "Cleaner.h"
-#include <map> 
-#include <string>
-//------------------------------------------------------------------- Constants
 
 //---------------------------------------------------------------------- PUBLIC
 
 //-------------------------------------------------------------- Public methods
 void Provider::addCleaner( Cleaner aCleaner)
 {
-    cleaners.insert(pair<string,Cleaner>(aCleaner.getId(),aCleaner));
-
 #ifdef MAP
     cout << "Calling for addMeasurement method of <Sensor>" << endl;
 #endif
+    cleaners.insert(pair<string,Cleaner>(aCleaner.getId(),aCleaner));
 }
 map<string,Cleaner> Provider::getCleanerList() const{
     return cleaners;
@@ -65,42 +62,25 @@ Provider::Provider ( const Provider & aProvider)
     cleaners = *(new map<string,Cleaner>(aProvider.cleaners));
 } //----- End of Provider (copy constructor)
 
-/*Provider::Provider ( string anId)
-{
-#ifdef MAP
-    cout << "Calling constructor of <Provider>" << endl;
-#endif
-    id= anId;
-} //----- End of Provider
-*/ //already done
 Provider::Provider ( )
-// Algorithm:
-//
 {
 #ifdef MAP
     cout << "Calling constructor of <Provider>" << endl;
 #endif
 } //----- End of Provider
 
-Provider::Provider ( string id)
-// Algorithm:
-//
+Provider::Provider ( string anId)
 {
-    this->id=id;
 #ifdef MAP
     cout << "Calling constructor of <Provider>" << endl;
 #endif
+    id = anId;
+
 } //----- End of Provider
 
 Provider::~Provider ( )
-// Algorithm:
-//
 {
 #ifdef MAP
     cout << "Calling destructor of <Provider>" << endl;
 #endif
 } //----- End of ~Provider
-
-//--------------------------------------------------------------------- PRIVATE
-
-//-------------------------------------------------------------- Private methods
