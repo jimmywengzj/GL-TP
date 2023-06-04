def18410cb14d09c7ef00f0cbe7bb06a16f4c9d7
arosard <alexandre@rosard.info>
2023-06-04 17:28:50 +0200

    Adding Testing for User class and correcting User class

---
+++ src/TestCleaner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexa_rhnokcr on 04/06/2023.
+//
+
+#include "TestCleaner.h"
---
+++ src/TestCleaner.h
@@ -0,0 +1,16 @@
+//
+// Created by alexa_rhnokcr on 04/06/2023.
+//
+
+#ifndef GL_TP_TESTCLEANER_H
+#define GL_TP_TESTCLEANER_H
+
+
+
+class TestCleaner {
+
+};
+
+
+
+#endif //GL_TP_TESTCLEANER_H
